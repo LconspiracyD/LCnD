7e3f9f6f6f16fa06a25b879d3490c93271626353
LconspiracyD <esanu.dominic@gmail.com>
2020-08-13 12:16:51 +0300

    new responsive header alpha v2

--- app.js
+++ app.js
@@ -1,27 +1,16 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
 
+burger.addEventListener("click", () => {
 
-
-    burger.addEventListener('click',()=>{
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ''
-             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`
-             }
-         });
-         //Burger Animation
-         burger.classList.toggle('toggle');
-
+    //Toggle Nav
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
     });
-    
-}
 
-navSlide();
+    //Burger Animation
+    burger.classList.toggle('toggle');
+    
+});
