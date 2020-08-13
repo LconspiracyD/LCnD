d4e0cf741970a9b47fde9467786b3adccb3679e8
LconspiracyD <esanu.dominic@gmail.com>
2020-08-13 21:03:55 +0300

    New Big Update! Beta 1

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
+const modal = document.querySelector('.modal');
+const previews = document.querySelectorAll('.gallery img');
+const original = document.querySelector('.full-img');
+const caption = document.querySelector('.caption');
 
 burger.addEventListener("click", () => {
 
@@ -13,4 +17,24 @@ burger.addEventListener("click", () => {
     //Burger Animation
     burger.classList.toggle('toggle');
 
+});
+
+previews.forEach(preview => {
+    preview.addEventListener('click', () => {
+        modal.classList.add('open');
+        original.classList.add('open');
+        //Dynamic change text and image
+        const originalSrc = preview.getAttribute("data-original");
+        original.src = `./screenshots/${originalSrc}`; 
+        const altText = preview.alt;
+        caption.textContent = altText;
+    });
+});
+
+modal.addEventListener('click', (e) => {
+    //Addon Remover
+    if (e.target.classList.contains('modal')) {
+        modal.classList.remove('open');
+        original.classList.remove('open');
+    }
 });
